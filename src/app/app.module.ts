eb33f9bb29f6c3c7de14b28d7270c42ae74c5397
nakuyabridget <bnakuya7@gmail.com>
2018-06-14 18:07:48 +0300

    FIRST PROJECT I WORKED ON USING ANGLAR JS

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { ServersComponent } from './servers/servers.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
